3e1eee022514f7b2951d81a3920ef7880b4fdcb4
Slicepotato <m.ianjohnson@gmail.com>
2023-03-21 12:23:56 -0400

    update js routing and pointing at main page

--- js/script.js
+++ js/script.js
@@ -1,33 +1,5 @@
-jQuery(document).ready(function($){
-    var detailsBox = document.getElementById('details-box');
-
-    document.addEventListener('mouseover', function (e) {
-    if (e.target.tagName == 'path') {
-        let content = e.target.dataset.name;
-        detailsBox.innerHTML = content;
-        detailsBox.classList.add('show');
-    }
-    else {
-        detailsBox.classList.remove('show');
-    }
-    });
-
-    window.onmousemove = function (e) {
-    let x = e.clientX,
-        y = e.clientY;
-        detailsBox.style.top = (y + 20) + 'px';
-        detailsBox.style.left = (x) + 'px';
-    };  
-    
+jQuery(document).ready(function($){ 
     $('.menu-item').on('click', 'a', function() {
         $(this).next('.sub-menu').toggleClass('show');
-    });
-
-    $('#state_select').on('change', function () {
-        var url = $(this).val(); // get selected value
-        if (url) { // require a URL
-            window.location = url; // redirect
-        }
-        return false;
     });
 });
