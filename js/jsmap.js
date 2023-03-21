5e9eef2c5467cdf44cd4415b6a7f2e9a7d9adda4
Slicepotato <m.ianjohnson@gmail.com>
2023-03-21 12:43:11 -0400

    recontextualize js placement

--- js/jsmap.js
+++ js/jsmap.js
@@ -1,24 +1,24 @@
-var detailsBox = document.getElementById('details-box');
-
-document.addEventListener('mouseover', function (e) {
-if (e.target.tagName == 'path') {
-    let content = e.target.dataset.name;
-    detailsBox.innerHTML = content;
-    detailsBox.classList.add('show');
-}
-else {
-    detailsBox.classList.remove('show');
-}
-});
-
-window.onmousemove = function (e) {
-let x = e.clientX,
-    y = e.clientY;
-    detailsBox.style.top = (y + 20) + 'px';
-    detailsBox.style.left = (x) + 'px';
-}; 
-
 jQuery(document).ready(function($){ 
+    let detailsBox = document.getElementById('details-box');
+
+    document.addEventListener('mouseover', function (e) {
+    if (e.target.tagName == 'path') {
+        let content = e.target.dataset.name;
+        detailsBox.innerHTML = content;
+        detailsBox.classList.add('show');
+    }
+    else {
+        detailsBox.classList.remove('show');
+    }
+    });
+
+    window.onmousemove = function (e) {
+    let x = e.clientX,
+        y = e.clientY;
+        detailsBox.style.top = (y + 20) + 'px';
+        detailsBox.style.left = (x) + 'px';
+    }; 
+
     $('#state_select').on('change', function () {
         var url = $(this).val(); // get selected value
         if (url) { // require a URL
