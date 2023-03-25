9616175cbc928b6ae7f25765217fd611d55d4d85
Michael Johnson <m.ianjohnson@gmail.com>
2023-03-25 16:58:54 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ContraCorners
+# ContraCorners 
+
+Theme file update `[2023]` for [ContraCorners.net](https://www.ContraCorners.net).
+
+## New Features
+- js/svg state map based on existence of state page in `states` Custom Post Type as generated through [CPT UI Plugin](https://wordpress.org/plugins/custom-post-type-ui/)
+- implementation of [EventOn](http://www.myeventon.com/) Calendar Plugin for listing dance events and their relavent information.
--- functions.php
+++ functions.php
@@ -151,4 +151,14 @@ function my_login_stylesheet() {
     // wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertext', 'my_login_logo_url_title' );
 ?>
