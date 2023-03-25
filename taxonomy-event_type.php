97e644b95a1d1236f66d93f5ef32fdb963f788ff
Michael Johnson <m.ianjohnson@gmail.com>
2023-03-25 18:06:50 -0400

    update dance weekend event category template

---
+++ taxonomy-event_type.php
@@ -0,0 +1,48 @@
+<?php get_header();
+
+	$tax = get_query_var( 'taxonomy' );
+	$term = get_query_var( 'term' );
+	$term = get_term_by( 'slug', $term, $tax );
+
+	$lang = isset($_GET['lang']) ? sanitize_text_field( $_GET['lang'] ): 'L1';
+
+	$tax_name = EVO()->frontend->get_localized_event_tax_names_by_slug($tax, $lang);
+	$term_name = evo_lang_get('evolang_'. $tax .'_'. $term->term_id, $term->name, $lang);
+
+	do_action('eventon_before_main_content');
+?>
+
+<div class='wrap evotax_term_card evotax_term_card alignwide'>
+	
+	<div id='' class="content-area">
+
+		<div class='eventon site-main'>
+		
+			<div class='entry-content'>
+				<div class='<?php echo apply_filters('evotax_template_content_class', 'eventon site-main');?>'>
+                    <h1 class="h1">Ready to Dance the Weekend Away?</h1>
+				
+					<div class="evotax_term_details endborder_curves" >					
+						<div class='tax_term_description'><?php echo category_description();?></div>
+					</div>
+
+                    <hr />
+
+					<?php 
+						$eventtop_style = EVO()->cal->get_prop('evosm_eventtop_style','evcal_1') == 'white'? '0':'2';
+						
+						$shortcode = apply_filters('evo_tax_archieve_page_shortcode', 
+							'[add_eventon_list number_of_months="5" '.$tax.'='.$term->term_id.' hide_mult_occur="no" hide_empty_months="yes" lang="'.$lang.'" eventtop_style="'. $eventtop_style.'"]', 
+							$tax,
+							$term->term_id
+						);
+						echo do_shortcode($shortcode);
+					?>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+
+<?php	do_action('eventon_after_main_content'); ?>
+<?php get_footer(); ?>
